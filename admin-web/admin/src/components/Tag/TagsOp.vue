5f463a6c95465f504461fce11656520ba8c9e9a6
xinghe98 <lcg9899@gmail.com>
2023-10-03 17:37:19 +0800

    tags compent

--- admin-web/admin/src/components/Tag/TagsOp.vue
+++ admin-web/admin/src/components/Tag/TagsOp.vue
@@ -16,18 +16,47 @@
 			</template>
 		</el-table-column>
 	</el-table>
-	<add-tag />
-	<!--TODO: 使用pinia 将addtag和edittag 复用为1个-->
-	<edit-tag />
+	<el-button class="btn" type="primary" @click="handleAdd">添加</el-button>
+	<!--INFO: 使用pinia 将addtag和edittag 复用为1个-->
+	<el-dialog v-model="dialogFormVisible" :title="title">
+		<el-form :model="form">
+			<el-form-item label="标签名" label-width="140px">
+				<el-input v-model="form.name" autocomplete="off" />
+			</el-form-item>
+		</el-form>
+		<template #footer>
+			<span class="dialog-footer">
+				<el-button @click="dialogFormVisible = false">取消</el-button>
+				<el-button type="primary" @click="addTag(form)"> 确认 </el-button>
+			</span>
+		</template>
+	</el-dialog>
 </template>
 
 <script lang="ts" setup>
-import editTag from "./editTag.vue";
-import { onMounted, ref, inject } from "vue";
+import { onMounted, ref, inject, reactive } from "vue";
 import request from "@/util/request";
 import { ElMessage } from "element-plus";
-import addTag from "./addTag.vue";
+import { tagsStore } from "@/store/tagsStore";
+
+const tags = tagsStore();
 const reload: any = inject("reload");
+const dialogFormVisible = ref(tags.isShow);
+const title = ref(tags.windowName);
+const form = reactive({
+	name: "",
+});
+
+const addTag = async (form: { name: string }) => {
+	try {
+		const res = await request.post("/tags/create", form);
+		ElMessage.success(res.data.msg);
+		dialogFormVisible.value = false;
+		reload();
+	} catch (e: any) {
+		ElMessage.error(e.response.data.msg);
+	}
+};
 interface tags {
 	name: string;
 	ID: number;
@@ -36,7 +65,9 @@ interface tags {
 const tableData = ref<tags[]>([]);
 
 const handleEdit = (row: tags) => {
-	console.log(row);
+	tags.editTag("编辑分类", row.ID);
+	dialogFormVisible.value = true;
+	title.value = tags.windowName;
 };
 const handleDelete = async (row: tags) => {
 	try {
@@ -48,6 +79,11 @@ const handleDelete = async (row: tags) => {
 	}
 };
 
+const handleAdd = () => {
+	tags.addTag("添加分类");
+	dialogFormVisible.value = true;
+	title.value = tags.windowName;
+};
 onMounted(async () => {
 	const res = await request.get("/tags/findall");
 	tableData.value = res.data.data;
@@ -59,5 +95,16 @@ onMounted(async () => {
 	width: 380px;
 	margin: 0 auto;
 	padding: auto;
+}
+.btn {
+	width: 380px;
+	margin: 10px auto;
+	padding: auto;
+}
+.dialog-footer button:first-child {
+	margin-right: 10px;
+}
+.el-input {
+	width: 300px;
 }
 </style>
